7321605283e6a218a1e4ee28a2739f06942490e7
lucasdoti <lucas_doti@hotmail.com>
2023-05-23 01:16:29 -0300

    subindo projeto

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const dia = document.getElementById('dia');
+const mes = document.getElementById('mes');
+
+const data = setInterval(function Date() {
+    dayName = new Array ("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado")
+    monName = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
+    now = new Date
+
+})
